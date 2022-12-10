e3044749a35a7d7cad606c5abad29d8916003c49
Caleb Soden <soden.caleb@gmail.com>
2022-12-09 23:18:09 -0600

    I think I realized my problem, rewriting again

--- 2022/Day_09/part2.py
+++ 2022/Day_09/part2.py
@@ -2,6 +2,7 @@ from itertools import chain
 import time
 
 USE_LOGGING = True
+SUPER_LOGGING = False
 USE_DEMO = True
 PART_ONE = False
 
@@ -20,9 +21,8 @@ tailHistories = [[(0,0)] for x in range(9)]
 def followCommands(commands):
     global headPosition
     
-    for c in commands[:2]:
+    for c in commands:
         if USE_LOGGING: print(c)
-        xMove = False
 
         match c.strip().split():
             case 'U', u: 
@@ -31,10 +31,8 @@ def followCommands(commands):
                 headPosition = (headPosition[0], headPosition[1] - int(d))
             case 'L', l:
                 headPosition = (headPosition[0] - int(l), headPosition[1])
-                xMove = True
             case 'R', r:
                 headPosition = (headPosition[0] + int(r), headPosition[1])
-                xMove = True
 
         localHeadPosition = headPosition
         
@@ -45,7 +43,7 @@ def followCommands(commands):
             #headX, headY = localHeadPosition[0], localHeadPosition[1]
             tailX, tailY = localTailPosition[0], localTailPosition[1]
 
-            if USE_LOGGING: print(f'\t\tKnot {knot + 1}: {localHeadPosition} - {localTailPosition} = {positionDiff}')
+            if SUPER_LOGGING: print(f'\t\tKnot {knot + 1}: {localHeadPosition} - {localTailPosition} = {positionDiff}')
 
             xmod = 1 if diffX > 0 else -1 if diffX < 0 else 0
             ymod = 1 if diffY > 0 else -1 if diffY < 0 else 0
@@ -66,22 +64,20 @@ def followCommands(commands):
             else:
                 xRange = list(range(1 * xmod, diffX + xmod, xmod))
                 yRange = list(range(1 * ymod, diffY + ymod, ymod))
-
-                if xMove: xRange = xRange[:-1]
-                else: yRange = yRange[:-1]
                 
                 diff = len(xRange) - len(yRange)
                 
                 if diff < 0:
-                    #print(xRange)
                     xRange += [xRange[-1]] * abs(diff)
                 elif diff > 0:
-                    #print(yRange)
                     yRange += [yRange[-1]] * diff
 
-            for i,j in zip(xRange, yRange):
+            for i,j in zip(xRange, yRange):                
+                if abs(localHeadPosition[0] - localTailPosition[0]) < 2 and abs(localHeadPosition[1] - localTailPosition[1]) < 2:
+                    break
+
                 localTailPosition = (tailX + i, tailY + j)
-                if USE_LOGGING: print(f'\t\t{(tailX, tailY)} + {(i,j)} = {localTailPosition}')
+                if SUPER_LOGGING: print(f'\t\t{(tailX, tailY)} + {(i,j)} = {localTailPosition}')
                 tailHistories[knot].append(localTailPosition)
 
             if USE_LOGGING: print(f'\tKnot {knot + 1} ended at {localTailPosition}')
@@ -103,7 +99,12 @@ if USE_LOGGING: print('---------------------------------------------------------
 
 commands = getInput(file)
 followCommands(commands)
-solution = len(list(set(tailHistories[-1])))
+seen = set()
+unique9set = [x for x in tailHistories[-1] if x not in seen and not seen.add(x)]
+if USE_LOGGING: 
+    for point in unique9set: 
+        print(point)
+solution = len(unique9set)
 
 endtime = time.time()
 print('Solution: ', solution)
