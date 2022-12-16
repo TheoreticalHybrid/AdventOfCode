06fde0baffefc710b2ea9eb9e24557a13db7c704
TheoreticalHybrid <soden.caleb@gmail.com>
2022-12-15 22:22:32 -0600

    Day 13 Solution

--- 2022/Day_13/main.py
+++ 2022/Day_13/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from functools import cmp_to_key
 import getopt
 import sys
 import time
@@ -29,12 +30,17 @@ def convertPacket(packet):
     while len(packet) > 0:
         if packet[0] == lBracket:            
             #find matching rBracket
-            listEndIndex = packet.find(rBracket) + 1
-            if listEndIndex < 0: raise Exception("Closing bracket not found")
-            listItem = packet[:listEndIndex]
-            internalList = convertPacket(listItem)
-            packetList.append(internalList)
-            packet = packet[listEndIndex:].strip()
+            bracketStack = []
+            for i,c in enumerate(packet):
+                if c == lBracket: bracketStack.append(c)
+                elif c == rBracket: bracketStack.pop()
+
+                if not bracketStack:
+                    listItem = packet[:i+1]
+                    internalList = convertPacket(listItem)
+                    packetList.append(internalList)
+                    packet = packet[i+1:].strip()
+                    break
         else:
             commaIndex = packet.find(splitCharacter)
 
@@ -50,116 +56,40 @@ def convertPacket(packet):
 
     return packetList
 
+def compareLists(leftList, rightList):
 
-def comparePackets2(leftPacket, rightPacket):
-    lPacket = convertPacket(leftPacket)
-    rPacket = convertPacket(rightPacket)
+    lLen,rLen = len(leftList),len(rightList)
+    listDiff = lLen - rLen # positive: left is longer, 0: equal length, negative: right is longer
 
-    return True
+    for i in range(min(lLen, rLen)):
+        l, r = leftList[i],rightList[i]
+        lType,rType = type(l),type(r)
+        
+        if lType is int and rType is int:
+            if l == r: continue
+            else: return l < r
+
+        if lType is int: l = [l]
+        elif rType is int: r = [r]
+
+        result = compareLists(l,r)
+        if result is None: continue
+        else: return result
+
+    #if we reached here, we ran out of items
+    return None if listDiff == 0 else listDiff < 0
+
 
 def comparePackets(leftPacket, rightPacket):
-    splitCharacter, lBracket, rBracket = ",", "[", "]"
-    
-    lPacket = [x.strip() for x in leftPacket.split(splitCharacter)]
-    rPacket = [x.strip() for x in rightPacket.split(splitCharacter)]
+    leftIsSmaller = compareLists(leftPacket, rightPacket)
 
-    # if USE_LOGGING:
-    #     print(lPacket)
-    #     print(rPacket)
-    #     print()
-
-    listStack = [] #might not actually be necessary to keep track of how deep the lists are
-
-    for i in range(max(len(lPacket), len(rPacket))):
-        if i == len(lPacket): 
-            if USE_LOGGING: print(f'\t\tRight packet was shorter')
-            return False #right packet was shorter
-        if i == len(rPacket):
-            if USE_LOGGING: print(f'\t\tLeft packet was shorter')
-            return True #left packet was shorter
-
-        l = lPacket[i]
-        r = rPacket[i]
-
-        while (len(l) > 0 or len(r) > 0):
-            if USE_LOGGING: print(f'\tComparing {l} and {r}')
-
-            if len(l) == 0: 
-                if USE_LOGGING: print(f'\t\tLeft chunk ran out')
-                return True
-            elif len(r) == 0:
-                if USE_LOGGING: print(f'\t\tRight chunk ran out')
-                return False
-
-            if l.isnumeric() and r.isnumeric():
-                if l == r: break
-                else:
-                    x = int(l) < int(r)
-                    if USE_LOGGING: print(f'\t\tNumeric comparison: Smaller is {l if x else r}')
-                    return x
-
-            if l[0] == lBracket and r[0] == lBracket:
-                listStack.append(lBracket)
-                l = l[1:]
-                r = r[1:]
-                continue
-
-            if l[0] == lBracket:
-                if r[0] == rBracket:
-                    if USE_LOGGING: print(f'\t\tLeft was start of list and Right was ending a list')
-                    return False
-                if r[0] != lBracket:
-                    r = f'[{r}]'
-                    continue
-            elif r[0] == lBracket:
-                if l[0] == rBracket:
-                    if USE_LOGGING: print(f'\t\Right was start of list and Left was ending a list')
-                    return True
-                if l[0] != lBracket: #redundant check, but whatever
-                    l = f'[{l}]'
-                    continue
-            
-            if l[0] == rBracket or r[0] == rBracket:
-                if l[0] == r[0]:
-                    listStack.pop()
-                    l = l[1:]
-                    r = r[1:]
-                    continue
-                else:
-                    x = l[0] == rBracket
-                    b = "Left" if x else "right"
-                    if USE_LOGGING: print(f'\t\t{b} list ended first')
-                    return x
-
-
-            if l[-1] == rBracket and r[-1] == rBracket:
-                listStack.pop()
-                l = l[:-1]
-                r = r[:-1]
-                continue
-            elif l[-1] == rBracket:
-                x = int(l[:l.index(rBracket)]) <= int(r)
-                if USE_LOGGING:
-                    if x:
-                        print(f'\t\tLeft List ended first and value was <= right value')
-                    else: 
-                        print(f'\\tLeft list ended first but value was greater than right value')
-                return x
-            elif r[-1] == rBracket:
-                x = int(l) < int(r[:r.index(rBracket)])
-                if USE_LOGGING:
-                    if x:
-                        print(f'\t\Right List ended first and left value was < right value')
-                    else: 
-                        print(f'\\Right list ended first value was greater than right value')
-                return x
-
-    return True
+    #it'd be better to rewrite compareLists to return these numbers but this is faster
+    return 0 if leftIsSmaller is None else -1 if leftIsSmaller else 1
 
 def getCorrectPackets(input):
     correctIndexes = []
     for i, packetPair in enumerate(input):
-        allG = comparePackets2(packetPair[0], packetPair[1])
+        allG = comparePackets(convertPacket(packetPair[0]), convertPacket(packetPair[1])) < 0
         
         if USE_LOGGING:
             keyword = 'CORRECT' if allG else 'WRONG'
@@ -169,33 +99,63 @@ def getCorrectPackets(input):
 
     return correctIndexes
 
+def findDecoderKey(input):
+    # first convert all packets
+    packets = []
+    for packetPair in input:
+        packets.append(convertPacket(packetPair[0]))
+        packets.append(convertPacket(packetPair[1]))
+    
+    # then add divider packets
+    divider1 = [[2]]
+    divider2 = [[6]]
+    packets.append(divider1)
+    packets.append(divider2)
+
+    # then organize packets
+    packets.sort(key=cmp_to_key(comparePackets))
+
+    # find index of divider packets
+    div1Index = packets.index(divider1) + 1
+    div2Index = packets.index(divider2) + 1
+
+    # multiply indeces together
+    return div1Index * div2Index
+
 def main(argv):
     global USE_DEMO
     global USE_LOGGING
+    global PART_ONE
     
     solution = 0
 
-    opts, args = getopt.getopt(argv, "el")
+    opts, args = getopt.getopt(argv, "elt")
     for opt, arg in opts:
         match opt:
             case "-e":
                 USE_DEMO = True
             case "-l":
                 USE_LOGGING = True
+            case "-t":
+                PART_ONE = False
     
-    USE_DEMO = True
-    USE_LOGGING = True
+    #USE_DEMO = True
+    #USE_LOGGING = True
+    #PART_ONE = False
 
     startTime = time.perf_counter()
 
     file = 'example.txt' if USE_DEMO else 'input1.txt'
 
     input = getInput(file)
-    correctPackets = getCorrectPackets(input)
 
     if PART_ONE:
+        correctPackets = getCorrectPackets(input)
         if USE_LOGGING: print(correctPackets)
         solution = sum(correctPackets) + len(correctPackets)
+    else:
+        solution = findDecoderKey(input)
+
 
     endtime = time.perf_counter()
 
