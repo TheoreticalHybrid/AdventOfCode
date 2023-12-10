3a17babef256559d40c4a88a9a6e3efc153a1a78
Caleb <soden.caleb@gmail.com>
2023-12-10 14:17:07 -0600

    AoC '23 Day 10 Part 2 - This could probably be condensed somehow, but I can't be bothered

--- 2023/Day_10/main.py
+++ 2023/Day_10/main.py
@@ -1,11 +1,11 @@
 import time
 
-USE_LOGGING = True
-USE_DEMO = True
+USE_LOGGING = False
+USE_DEMO = False
 PART_ONE = False
 
-characterMap = {'|': (0,1), '-': (1,0), 'L': (1,-1), 'J': (-1,-1), '7': (-1,1), 'F': (1,1)}
 startingPoint = (0,0)
+loop = []
 
 def getInput(fileName):
     global startingPoint
@@ -26,7 +26,7 @@ def getInput(fileName):
 
     return input
 
-def moveLeft(c):
+def moveLeft(c): # determine the next direction of movement if coming from the left
     match c:
         case '-':
             return 'l'
@@ -35,7 +35,7 @@ def moveLeft(c):
         case 'F':
             return 'd'
 
-def moveRight(c):
+def moveRight(c): # determine the next direction of movement if coming from the right
     match c:
         case '-':
             return 'r'
@@ -44,7 +44,7 @@ def moveRight(c):
         case 'J':
             return 'u'
 
-def moveUp(c):
+def moveUp(c): # determine the next direction of movement if coming from below
     match c:
         case '|':
             return 'u'
@@ -53,7 +53,7 @@ def moveUp(c):
         case 'F':
             return 'r'
 
-def moveDown(c):
+def moveDown(c): # determine the next direction of movement if coming from above
     match c:
         case '|':
             return 'd'
@@ -65,33 +65,58 @@ def moveDown(c):
 def findFirstMove():
     myIndex = startingPoint
     ix, iy = myIndex[0], myIndex[1]
-    if iy > 0 and input[ix][iy-1] in ('-', 'L', 'F'): # connects to left cell
+    if iy > 0 and input[ix][iy-1] in ('-', 'L', 'F'): # if starting point has a connecting pipe to the left
         return 'l'
-    elif iy < len(input[0]) and input[ix][iy+1] in ('-', 'J', '7'): # connects to right cell
+    elif iy < len(input[0]) and input[ix][iy+1] in ('-', 'J', '7'): # if starting point has a connecting pipe to the right
         return 'r'
-    elif ix > 0 and input[ix-1][iy] in ('|', '7', 'F'): # connects to above cell
+    elif ix > 0 and input[ix-1][iy] in ('|', '7', 'F'): # if starting point has a connecting pipe above
         return 'u'
-    elif ix < len(input) and input[ix+1][iy] in ('|', 'L', 'J'): # connects to below cell
+    elif ix < len(input) and input[ix+1][iy] in ('|', 'L', 'J'): # if starting point has a connecting pipe below
         return 'd'
 
-def buildLoop(input):
-    loop = [('S', startingPoint)]
-    d = findFirstMove()
+def buildLoop(input): # builds the global loop list and returns a new map that changes all non-loop characters to '.'
+    global loop
 
+    loop = [('S', startingPoint)]
+    newInput = [['.'] * len(input[0]) for i in range(len(input))] # Build a new map that will be . except for the loop characters (cuts out the noise)
+
+    d = findFirstMove()
+    d1 = d # store for later pipe identification
     shifts = {'u': (-1, 0), 'd': (1, 0), 'r': (0, 1), 'l': (0, -1)}
 
     myIndex = startingPoint
     returnedToStart = False
     while not returnedToStart:
         s = shifts[d]
-        sx, sy = myIndex[0] + s[0], myIndex[1] + s[1]
-        sc = input[sx][sy]
-        loop.append((sc, (sx,sy)))
+        sx, sy = myIndex[0] + s[0], myIndex[1] + s[1] # get the x,y for the next pipe
+        sc = input[sx][sy] # get the next pipe
+        loop.append((sc, (sx,sy))) # add next pipe to the loop as (pipeCharacter, (pipeX, pipeY))
         myIndex = (sx,sy)
 
-        if sc == 'S':
+        if sc == 'S': # if the next pipe is the starting point
             returnedToStart = True
-        else:
+            match d: # determine the underlying pipe depending on what direction took us here and what direction we started with
+                case 'u':
+                    match d1:
+                        case 'u': sc = '|'
+                        case 'r': sc = 'F'
+                        case 'l': sc = '7'
+                case 'd':
+                    match d1:
+                        case 'd': sc = '|'
+                        case 'r': sc = 'L'
+                        case 'l': sc = 'J'
+                case 'r':
+                    match d1:
+                        case 'r': sc = '-'
+                        case 'u': sc = 'J'
+                        case 'd': sc = '7'
+                case 'l':
+                    match d1:
+                        case 'l': sc = '-'
+                        case 'u': sc = 'L'
+                        case 'd': sc = 'F'
+        else: # otherwise determine what direction the next pipe takes
             match d:
                 case 'u':
                     d = moveUp(sc)
@@ -101,44 +126,46 @@ def buildLoop(input):
                     d = moveRight(sc)
                 case 'l':
                     d = moveLeft(sc)
+        
+        newInput[sx][sy] = sc # update new map with pipe character
+    
+    if USE_LOGGING: 
+        for row in newInput: print(''.join(row))
 
-    return loop
+    return newInput # return new map
 
-# I don't know if there are irrelevant pipes within the loop, and I need to
-# remove the distractions for analysis
-def replaceGarbagePipes(input, loop):
-    newInput = []
-
-    for i,row in enumerate(input):
-        newRow = []
-        for j,c in enumerate(row):
-            newC = 'X'
-            if c in ('.', 'S'): newC = c
+def findEnclosedArea(input): # counts all '.' characters when it's between encasing pipe segments
+    total = 0
+    
+    # evaluate each map character from top to bottom, left to right
+    for row in input[1:-1]: # for each row besides the first and last (impossible to have valid '.' characters there)
+        inside = False
+        horizontalPipeEnd = None
+        for c in row: # for each character in that row
+            if c == '.':
+                if inside:
+                    total += 1
+            elif c == '|': # '|' character always toggles the inside indicator
+                inside = not inside
             else:
-                for pipe in filter(lambda p: p[0] == c, loop):
-                    if pipe[1][0] == i and pipe[1][1] == j:
-                        newC = c
-            newRow.append(newC)
+                if c in ('L', 'F'): # L and F always toggle the inside character
+                    inside = not inside
+                    horizontalPipeEnd = c # store the character as the beginning of a horizontal pipe segment
+                
+                # J and 7 only toggle the inside character if their vertical trajectory is different than that of the start of the horizontal pipe segment
+                # For example, F---7 both have the vertical direction pointing downward, but F---J has F pointing downward but J pointing upward
+                elif c in ('J', '7'):
+                    inside = inside if horizontalPipeEnd == ('F' if c == 'J' else 'L') else not inside
 
-        newInput.append(newRow)
-
-    return newInput
-
-def findEnclosedArea(loop):
-    pass
+    return total
 
 startTime = time.time()
 
 file = ('example1.txt' if PART_ONE else 'example2.txt') if USE_DEMO else 'input1.txt'
 input = getInput(file)
-loop = buildLoop(input)
+input = buildLoop(input)
 
-solution = 0
-if PART_ONE: solution = len(loop) // 2
-else:
-    input = replaceGarbagePipes(input, loop)
-    if USE_LOGGING: 
-        for row in input: print(''.join(row))
+solution = len(loop) // 2 if PART_ONE else findEnclosedArea(input)
 
 endtime = time.time()
 print(f'Solution: ', solution)
