92842310157896e99d669d36cb531e3fc2cf1dda
Caleb <soden.caleb@gmail.com>
2024-12-11 11:48:03 -0600

    AoC 24 Day 11 failed efficiency improvements iteration

--- 2024/Day_11/main.py
+++ 2024/Day_11/main.py
@@ -16,29 +16,100 @@ def getInput(fileName):
 
     return input
 
-def witnessChanges(input, blinkCount):
-    newList = []
-
-    for i in range(blinkCount):
-        iStart = time.time()
-        newList = []
-        for s in input:
-            if s == 0:
-                newList.append(1)
+def stoneChange(stoneValue):
+    newValue = []
+    if stoneValue == 0:
+        newValue = [1]
+    else:
+            sn = str(stoneValue)
+            snl = len(sn)
+            if snl % 2 == 0:
+                newValue = [int(sn[0:snl//2]), int(sn[snl//2:])]
             else:
-                sn = str(s)
-                snl = len(sn)
-                if snl % 2 == 0:
-                    n1, n2 = int(sn[0:snl//2]), int(sn[snl//2:])
-                    newList.append(n1)
-                    newList.append(n2)
-                else:
-                    newList.append(s * 2024)
-        input = newList
-        iEnd = time.time()
-        if USE_LOGGING: print(f'Blink {i} time: {iEnd - iStart}')
+                newValue = [stoneValue * 2024]
 
-    return newList
+    return newValue
+
+def witnessChanges4(input, blinkCount):
+    calcDict = dict()
+
+    count = 0
+    for value in input:
+        values = [(value, blinkCount)]
+        queueLength = 0
+        while any(values):
+            stoneValue, remainingBlinks = values.pop(0)
+            count += 1
+
+            foundNewValues = False
+            for i in range(remainingBlinks):                
+                newValue = None
+                if stoneValue in calcDict:
+                    newValue = calcDict[stoneValue]
+                else:
+                    newValue = stoneChange(stoneValue)
+                    calcDict[stoneValue] = newValue
+                    foundNewValues = True
+                    
+                stoneValue = newValue[0]
+                if len(newValue) > 1: values.append((newValue[1], remainingBlinks - (i + 1)))
+            
+            if USE_LOGGING: 
+                currentLength = len(values)
+                threshold = 500
+                if currentLength > queueLength + threshold or currentLength < queueLength - threshold:
+                    print(f'NewValues: {foundNewValues} - Queue Length - {currentLength}                  ', end='\r')
+                    queueLength = currentLength
+
+    return count
+
+def witnessChanges3(input, blinkCount):
+    calcDict = dict()
+
+    for value in input:
+        testValues = {value}
+        visitedValues = set()
+        for i in range(blinkCount):
+            if not any(testValues):
+                print(f'{value} has no more unique values to test at blink {i}')
+                break
+            iStart = time.time()
+            iValues = set(testValues)
+            testValues = set()
+            for s in iValues:
+                if s in visitedValues:
+                    print(f'{value} after blink {i} found a repeat value ({s})')
+                else:
+                    newValue = stoneChange(s)
+
+                    if USE_LOGGING: print(f'Blink {i}: {s} -> {newValue}')
+                    calcDict[s] = newValue
+                    testValues.update(newValue)
+                    visitedValues.add(s)
+            
+            iEnd = time.time()
+            print(f'Blink {i} time: {iEnd - iStart}')
+
+    stoneCount = 0
+    print(f'Counting Now')
+
+    for value in input:
+        stones = [value]
+        stoneCount += 1
+        for i in range(blinkCount):
+            subStones = []
+            for stone in stones:
+                subStones = subStones + calcDict[stone]
+            
+            stoneCount += len(subStones)
+            stones = subStones
+
+    return stoneCount
+
+def loopTest(value):
+    loop = [value]
+
+    
   
 file = 'example.txt' if USE_DEMO else 'input1.txt'
 input = getInput(file)
@@ -46,20 +117,18 @@ input = getInput(file)
 startTime = time.time()
 
 numberOfBlinks = 5 if USE_DEMO else 25
-stones = witnessChanges(input, numberOfBlinks)
-solution = len(stones)
+solution = witnessChanges4(input, numberOfBlinks)
 
 endtime = time.time()
 print(f'Part 1 Solution: ', solution)
 print('Part 1 Completion time: ', endtime - startTime)
 
 #exit()
-if USE_LOGGING: print('---------PART TWO---------')
+print('---------PART TWO---------')
 startTime = time.time()
 
 numberOfBlinks = 75
-stones = witnessChanges(input, numberOfBlinks)
-solution = len(stones)
+solution = witnessChanges4(input, numberOfBlinks)
 
 endtime = time.time()
 print(f'Part 2 Solution: ', solution)
