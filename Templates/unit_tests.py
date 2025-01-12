53d8744036ed1afc28ef18a302a672c6cfd18323
Caleb <soden.caleb@gmail.com>
2025-01-12 09:15:33 -0600

    Updated unit tests template

--- Templates/unit_tests.py
+++ Templates/unit_tests.py
@@ -3,20 +3,22 @@ import solution
 
 class TestMain(unittest.TestCase):
     def test_part1(self):
+        testCount = 1
         testCases = []
         #testCases.append(('', 0))
         
         for testValue, expectedResult in testCases:
-            #self.assertEqual(solution.getEndingFloor(testValue), expectedResult, msg=f'Test {testValue} failed')
-            pass
+            #self.assertEqual(solution.getEndingFloor(testValue), expectedResult, msg=f'Test {testCount} ({testValue}) failed')
+            testCount += 1
 
     def test_part2(self):
+        testCount = 1
         testCases = []
         #testCases.append(('', 0))
 
         for testValue, expectedResult in testCases:
-            #self.assertEqual(solution.getFirstBasementIndex(testValue), expectedResult, msg=f'Test {testValue} failed')
-            pass
+            #self.assertEqual(solution.getFirstBasementIndex(testValue), expectedResult, msg=f'Test {testCount} ({testValue}) failed')
+            testCount += 1
 
 if __name__ == "__main__":
     unittest.main()
